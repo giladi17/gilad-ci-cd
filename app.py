c350cd0aad1305fe88dbee4dbaf3dee3619967be
giladi17 <giladi17@gmail.com>
2026-03-22 21:37:00 +0200

    Update greeting message in home route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "hello from gilad11!"
+    return "hello from gilad11111!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
